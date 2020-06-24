7331347119a2d6eb31831dd16e410e2aff1253e0
Maick Souza <maick_a_s@msn.com>
2020-06-24 20:57:19 -0300

    refactor(server): recovering password

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,6 +1,6 @@
 import { uuid } from 'uuidv4';
 
-import IUsersRepository from '@modules/users/repositories/IUSersRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 import User from '../../infra/typeorm/entities/User';
