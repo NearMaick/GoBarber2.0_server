1fdb94cc6a6ebca707e0747682dc5e41eafb97d6
Maick Souza <maick_a_s@msn.com>
2020-06-25 22:43:50 -0300

    test(server): reseting password

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -16,6 +16,16 @@ class FakeUserTokensRepository implements IUserTokensRepository {
       user_id,
     });
 
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
+
     return userToken;
   }
 }
