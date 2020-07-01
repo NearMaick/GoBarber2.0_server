bc1d3cee5e5bfa471db35d78ba9a81545d028bde
Maick Souza <maick_a_s@msn.com>
2020-06-30 21:06:33 -0300

    feat(server): developing email service #2 template

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,12 +6,20 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 import ImailProvider from './MailProvider/models/ImailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
+import ImailTemplateProvider from './MailTemplateProvider/models/IMailTemplatePovider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
+container.registerSingleton<ImailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
+
 container.registerInstance<ImailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  container.resolve(EtherealMailProvider),
 );
